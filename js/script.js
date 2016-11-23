7dbb200d9809fb69dba9b735fe1e69786ae26878
NewtonKamau <kamaunewton78@gmail.com>
2016-11-23 08:26:56 +0300

    add file with js file

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var add= function (number1, number2) {retun number1 + number2;};
+var number1=parseInt prompt("Enter the first number");
+var number2=parseInt prompt("Enter the second number");
+alert(add(number1, number2));
+
+var minus= function (number1, number2) {return number1 - number2;};
+var number1parseInt prompt("Enter the first number");
+var number1parseInt prompt("Enter the second number");
+alert(minus(number1, number2));
+
+var multiply= function (number1, number2) {return number1 * number2;};
+var number1=parseInt prompt("Enter the first number");
+var number2=parseInt prompt("Enter the second number");
+alert(multiply(number1, number2));
+
+var divide= function (number1, number2) {return number1 / number2;};
+var number1=parseInt prompt("Enter the first number");
+var number2=parseInt prompt("Enter the second number");
+alert(divide(number1, number2));
